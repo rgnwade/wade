b1ea2a04e7e7f0c146d252483545d10eb8e901a8
Regina Ayu Prameswari Wade <reginawade.rgn@gmail.com>
2018-02-22 16:02:55 +0800

    2/22/2018

--- laracode/resources/views/home/welcome.blade.php
+++ laracode/resources/views/home/welcome.blade.php
@@ -16,4 +16,32 @@
    <button class="button-masuk button-besar">Mulai Sekarang</button>
    
    </div>
+
+
+
+
+   <div class="kategori container">
+
+   	<div class="kat-title">
+	KATEGORI
+
+</div>
+
+<div class="kat-img">
+
+   	<div class="kat-img-1 col-md-4 no-gap">
+   <img class="katimg-1"  src="{{url('public/uploads/kat-1.jpg')}}" />
+	</div>
+
+<div class="kat-img-1 col-md-4 no-gap">
+   <img class="katimg-2"  src="{{url('public/uploads/kat-2.jpg')}}" />
+	</div>
+
+<div class="kat-img-1 col-md-4 no-gap">
+   <img class="katimg-3"  src="{{url('public/uploads/kat-3.jpg')}}" />
+	</div>
+
+   </div>
+</div>
+
 @endsection
